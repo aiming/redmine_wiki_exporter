56784e46820038b6852014005763c6438af86600
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2011-09-07 16:17:18 +0900

    fixes spec fails

--- spec/redmine_wiki_exporter_spec.rb
+++ spec/redmine_wiki_exporter_spec.rb
@@ -1,16 +1,15 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "RedmineWikiExporter" do
-  describe ".self" do
-    context "#url_to_wikiname" do
-      context do
-        subject { RedmineWikiExporter.url_to_wikiname("http://foo/bar/baz") }
-        it { should == "baz" }
-      end
-      context do
-        subject { RedmineWikiExporter.url_to_wikiname("http://foo/bar/") }
-        it { should == "bar" }
-      end
+  context "#url_to_wikiname" do
+    let(:exporter) { RedmineWikiExporter.new(nil, nil) }
+    context do
+      subject { exporter.url_to_wikiname("http://foo/bar/baz") }
+      it { should == "baz" }
+    end
+    context do
+      subject { exporter.url_to_wikiname("http://foo/bar/") }
+      it { should == "bar" }
     end
   end
 end
